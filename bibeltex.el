631e79ff146518e8ba44405d49ad7673128a3064
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2013-06-15 03:08:33 +0200

    New style format.
    Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

--- bibeltex.el
+++ bibeltex.el
@@ -87,6 +87,7 @@ See `bibeltex-style-alist' for potential styles."
   :group 'bibeltex
   :type '(list (cons (symbol :tag "Style name")
                      (variable :tag "Style variable"))))
+;;;###autoload (put bibeltex-style-alist 'risky-local-variable t)
 
 (defcustom bibeltex-format-cite-entry "[[#%l][%n]]"
   "How for format individual cite entries.
@@ -224,79 +225,29 @@ entries from the list."
 
 ;;; Style
 
-;; Code inspired by reftex-cite.el
+(defun bibeltex--split-names (names)
+  "Split NAMES into a list."
+  (setq names (replace-regexp-in-string "\\b[eE]t[ \t]+al\\."
+                                        "and /et al./" names))
+  (split-string names "[ \t]*\\band\\b[ \t]*" 'omit-nulls))
 
-(defun bibeltex--get-names (field entry)
-  "Return list of names for FIELD in ENTRY."
-  (let ((names (bibeltex--get-field field entry)))
-    (setq names (replace-regexp-in-string "\\b[eE]t[ \t]+al\\."
-                                          "and /et al./" names))
-    (split-string names "[ \t]*\\band\\b[ \t]*" 'omit-nulls)))
-
-(defconst bibeltex--format-map
-  '((?l . :key)
-    (?L . :key-num)
-    (?n . :number)
-    (?v . :volume)
-    (?s . :school)
-    (?u . :publisher)
-    (?r . :address)
-    (?U . :url)
-    (?t . :title)
-    (?p . :pages)
-    (?o . :organization)
-    (?N . :note)
-    (?j . :journal)
-    (?i . :institution)
-    (?h . :howpublished)
-    (?b . :booktitle)
-    (?d . :edition)
-    (?D . :doi)
-    (?c . :chapter)
-    (?m . :month)
-    (?y . :year))
-  "Map format spec to field name.")
-
-(defun bibeltex--format-names (field entry &optional num)
-  "Format NUM names for FIELD in ENTRY.
+(defun bibeltex--style-names (names &optional num)
+  "Format NUM names from NAMES.
+If NUM is a string it is converted to a number.
 If NUM is not a number format all names.
 If names are cut off they are replaced by \"et al.\""
-  (let ((names (bibeltex--get-names field entry))
-        str)
-    (when (numberp num)
-      (setq names (org-sublist names 1 num)))
-    (setq str
-          (mapconcat #'identity
-                     names
-                     ", "))
+  (setq names (bibeltex--split-names names))
+  (when (stringp num)
+    (setq num (string-to-number num)))
+  (when (numberp num)
+    (setq names (org-sublist names 1 num)))
+  (let ((str
+         (mapconcat #'identity
+                    names
+                    ", ")))
     (if (and (numberp num) (< num (length names)))
         (concat str ", /et al./")
       str)))
-
-(defun bibeltex--format-entry (format entry)
-  "Format ENTRY according to FORMAT."
-  (while (string-match "%\\([0-9]*\\)\\([a-zA-Z%]\\)" format)
-    (let ((num (string-to-number (match-string 1 format)))
-          (spec (string-to-char (match-string 2 format))))
-      (setq format
-            (replace-match
-             (save-match-data
-               (cl-case spec
-                 (?% "%") ;; %%
-                 (?a (bibeltex--format-names :author entry (when (/= num 0) num)))
-                 (?A (car (bibeltex--get-names :author entry)))
-                 (?e (bibeltex--format-names :editor entry num))
-                 (?E (car (bibeltex--get-names :editor entry)))
-                 (?P (car (split-string (bibeltex--get-field :pages entry)
-                                        "[- .]+")))
-                 (otherwise ;; use `bibeltex-format-map'
-                  (let ((field (cdr (assq spec bibeltex--format-map))))
-                    (unless field
-                      (error "Invalid format string: %s"
-                             (match-string 0 format)))
-                    (bibeltex--get-field field entry)))))
-             'fixed-case 'literal format))))
-  format)
 
 (defgroup bibeltex-styles nil
   "Styles for BibELTeX."
@@ -304,10 +255,84 @@ If names are cut off they are replaced by \"et al.\""
   :prefix "bibeltex-style")
 
 (defcustom bibeltex-style-default
-  '((article . "** =[%L]= %2a, %y: %t\n:PROPERTIES:\n:CUSTOM_ID: %l\n:END:\n\n- %y %m\n- [[%U]]\n- DOI: [[doi://%D][%D]]\n\n")
-    (t . "** =[%L]= %2a, %y: %t\n:PROPERTIES:\n:CUSTOM_ID: %l\n:END:\n\n"))
+  '("** =[%key-num%]= %author:names(2)%, %year%: %title%\n"
+    ":PROPERTIES:\n:CUSTOM_ID: %key%\n:END:\n\n- %year% %month%\n"
+    "- [[%url:orcancel%]]\n"
+    "- DOI: [[doi://%doi:orcancel%]]\n"
+    "\n")
   "Default style."
   :group 'bibeltex-styles)
+;;;###autoload (put bibeltex-style-default 'risky-local-variable t)
+
+(defun bibeltex--style-orcancel (val)
+  "If VAL is null or empty string cancel entry."
+  (if (or (string= val "")
+          (null val))
+      'cancel
+    val))
+
+(defvar bibeltex--style-option-functions '(("names" . bibeltex--style-names)
+                                           ("orcancel" . bibeltex--style-orcancel))
+  "Map style function names to elisp functions.
+The functions get called with the entry value as first argument and any style
+arguments after that.  The function should return a string or `cancel' to
+cancel the part of the entry.")
+
+(defun bibeltex--style-format (str entry)
+  "Do the STR format for ENTRY.
+The control strings look like %KEY% or with options %KEY:OPTION1:OPTION2:...%.
+The KEY should be the name of an entry without the leading :.
+E.g., %key-num% will be replaced by the value of the :key-num field.
+OPTIONS are of the format FUN(PARAM1,PARAM2,...).  With FUN being any function
+name listed in `bibeltex--style-option-functions'."
+  (let (start)
+    (while (setq start (string-match "%\\([^%]+\\)%" str start))
+      (let* ((data (save-match-data
+                     (split-string (match-string 1 str) ":" 'omit-nulls)))
+             (key (intern (concat ":" (or (car data) "x"))))
+             (value (or (cdr (assq key entry)) "")))
+        (save-match-data
+          ;; handle options
+          (dolist (str (cdr data))
+            (when (string-match "\\([^(]+\\)\\((\\(.+\\))\\)?" str)
+              (let* ((func-name (match-string 1 str))
+                     (func (cdr (assoc func-name bibeltex--style-option-functions)))
+                     (args (split-string (or (match-string 3 str) "")
+                                         "," 'omit-nulls)))
+                (when func
+                  (setq value (apply func value args)))))))
+        (if (eq value 'cancel)
+            (setq str ""
+                  start nil)
+          (unless (stringp value)
+            (setq value ""))
+          (setq str (replace-match value 'fixed 'literal str))
+          (setq start (+ start (length value)))))))
+  str)
+
+(defun bibeltex--style-let-entry (entry x)
+  "Eval X with the values of ENTRY bound.
+The fields are named entry-FIELDNAME.  E.g., the :key-num field will
+become entry-key-num."
+  (eval
+   `(let ,(mapcar (lambda (x)
+                    (list (intern
+                           (concat "entry-"
+                                   ;; remove :
+                                   (substring (symbol-name (car x)) 1)))
+                          (cdr x)))
+                  entry)
+      (eval x))))
+
+(defun bibeltex--style-do (style entry)
+  "Handle STYLE for ENTRY."
+  (apply #'concat
+         (mapcar
+          (lambda (x)
+            (unless (stringp x)
+              (setq x (bibeltex--style-let-entry entry x)))
+            (bibeltex--style-format x entry))
+          style)))
 
 (defun bibeltex--get-style (style)
   "Get STYLE format instructions."
@@ -316,22 +341,14 @@ If names are cut off they are replaced by \"et al.\""
       (error "Unknown style `%s' not found in `bibeltex-style-alist'" style))
     (symbol-value style-var)))
 
-(defun bibeltex--style-handle-entry (entry style)
-  "Write ENTRY according to STYLE."
-  (let* ((type-tmp (cdr (assq :type entry)))
-         (type (intern (downcase type-tmp)))
-         (format (cdr (or (assq type style)
-                          (assq t style)))))
-    (insert (bibeltex--format-entry format entry))
-    (unless (bolp) ;; Make sure we end with a newline
-      (insert "\n"))))
-
 (defun bibeltex--use-style (style)
   "Use STYLE to format `org-bibtex-entries'."
   (setq style (bibeltex--get-style style))
   (insert "\n* Bibliography\n")
   (dolist (entry org-bibtex-entries)
-    (bibeltex--style-handle-entry entry style)))
+    (insert (bibeltex--style-do style entry))
+    (unless (bolp) ;; Make sure we end with a newline
+      (insert "\n"))))
 
 (provide 'bibeltex)
 
